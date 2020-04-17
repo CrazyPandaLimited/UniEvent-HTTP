908340218d950c0474db4ef685d738cf72fbafde
syber <syber@crazypanda.ru>
2020-04-17 15:39:38 +0300

    UniEvent::HTTP: cosmetic

--- plus/UniEvent-HTTP/src/panda/unievent/http/ServerConnection.cc
+++ plus/UniEvent-HTTP/src/panda/unievent/http/ServerConnection.cc
@@ -239,7 +239,7 @@ void ServerConnection::drop_requests (const ErrorCode& err) {
 void ServerConnection::close (const ErrorCode& err, bool soft) {
     panda_log_notice("close: " << err);
     ServerConnectionSP hold = this; (void)hold;
-    ServerSP holdSrv = server; (void)holdSrv;
+    ServerSP hold_srv = server; (void)hold_srv;
 
     event_listener(nullptr);
 
